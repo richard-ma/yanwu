fc9fb1b5004de335cd2f9b23bbb6050f676bfd42
richard-ma <richard.ma.19850509@gmail.com>
2022-05-07 09:39:49 +0800

    init commit

---
+++ src/shredder/App.py
@@ -0,0 +1,92 @@
+# !/usr/bin/env python
+# APP Framework 1.0
+import copy
+import csv
+import os
+import sys
+import shutil
+import requests
+from pprint import pprint
+
+
+class App:
+    def __init__(self):
+        self.title_line = sys.argv[0]
+        self.counter = 1
+        self.workingDir = None
+
+    def printCounter(self, data=None):
+        print("[%04d] Porcessing: %s" % (self.counter, str(data)))
+        self.counter += 1
+
+    def initCounter(self, value=1):
+        self.counter = value
+
+    def run(self):
+        self.usage()
+        self.process()
+
+    def usage(self):
+        print("*" * 80)
+        print("*", " " * 76, "*")
+        print(" " * ((80 - 12 - len(self.title_line)) // 2),
+              self.title_line,
+              " " * ((80 - 12 - len(self.title_line)) // 2))
+        print("*", " " * 76, "*")
+        print("*" * 80)
+
+    def input(self, notification, default=None):
+        var = input(notification)
+
+        if len(var) == 0:
+            return default
+        else:
+            return var
+
+    def readTxtToList(self, filename, encoding="GBK"):
+        data = list()
+        with open(filename, 'r+', encoding=encoding) as f:
+            for row in f.readlines():
+                # remove \n and \r
+                data.append(row.replace('\n', '').replace('\r', ''))
+        return data
+
+    def readCsvToDict(self, filename, encoding="GBK"):
+        data = list()
+        with open(filename, 'r+', encoding=encoding) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                data.append(row)
+        return data
+
+    def writeCsvFromDict(self, filename, data, fieldnames=None, encoding="GBK", newline=''):
+        if fieldnames is None:
+            fieldnames = data[0].keys()
+
+        with open(filename, 'w+', encoding=encoding, newline=newline) as f:
+            writer = csv.DictWriter(f,
+                                    fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
+    def addSuffixToFilename(self, filename, suffix):
+        filename, ext = os.path.splitext(filename)
+        return filename + suffix + ext
+
+    def getWorkingDir(self):
+        return self.workingDir
+
+    def setWorkingDir(self, wd):
+        self.workingDir = wd
+        return self.workingDir
+
+    def setWorkingDirFromFilename(self, filename):
+        return self.setWorkingDir(os.path.dirname(filename))
+
+    def process(self):
+        print("App Run!")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
