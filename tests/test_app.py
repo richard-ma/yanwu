8232859d94c110c964eec9c873c74c196c711779
richard-ma <richard.ma.19850509@gmail.com>
2022-07-27 10:35:59 +0800

    create package and test it

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
-import pytest
-import yanwu
+from yanwu import App
 
 
-def test_app():
-    return True
+def test_init():
+    app = App()
+    assert app.counter == 1
+    assert app.workingDir is None
+
+
+if __name__ == "__main__":
+    print(dir(App))
