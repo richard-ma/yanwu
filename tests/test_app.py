8c8aae4d7a80e63186dc75b0da0f45bfc9bdf511
richard-ma <richard.ma.19850509@gmail.com>
2022-10-29 11:34:27 +0800

    first test successful

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,7 @@
 from yanwu import App
 
 
-def test_init():
+def test_init_app():
     app = App()
     assert app.counter == 1
     assert app.workingDir is None
-
-
-if __name__ == "__main__":
-    print(dir(App))
